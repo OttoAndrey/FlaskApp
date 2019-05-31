3d5db1b2ca8098e1c6cc37ed06b2001e0f063816
Ray <ottotruepro@gmail.com>
2019-05-31 17:17:57 +0700

    Основа

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from flask import Flask
-
-
-app = Flask(__name__) #экземпляр класса flask
-
-
-@app.route('/')
-def index():
-    return '<h1>kek</h1>'
+from app import app
+import view
 
 if __name__ == '__main__':
     app.run()
