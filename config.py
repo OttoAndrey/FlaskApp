ef1a5d88a6d65516dc0f56dd43fad0aadcd9f696
Ray <ottotruepro@gmail.com>
2019-06-20 12:00:31 +0700

    update for heroku

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+import os
+
+
 class Configuration(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/test'
-    SECRET_KEY = 'на самом деле не оч секретный какаой-то'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SECRET_KEY = os.environ['SECRET_KEY']
     SECURITY_PASSWORD_SALT = 'salt'
     SECURITY_PASSWORD_HASH = 'sha512_crypt'
-
