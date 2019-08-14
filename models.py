2ce118f05ecd2f2f8cb842c1bcde35ffac551473
Ray <ottotruepro@gmail.com>
2019-08-14 12:21:30 +0700

    update model Tag

--- models.py
+++ models.py
@@ -38,11 +38,15 @@ class Post(db.Model):
 class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    slug = db.Column(db.String(100))
+    slug = db.Column(db.String(100), unique=True)
 
     def __init__(self, *args, **kwargs):
         super(Tag, self).__init__(*args, **kwargs)
-        self.slug = slugify(self.name)
+        self.generate_slug()
+
+    def generate_slug(self):
+        if self.name:
+            self.slug = slugify(self.name)
 
     def __repr__(self):
         return '{0}'.format(self.name)
