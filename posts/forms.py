3b98de2e1abbe967cd728c8cad583135f17cec92
Ray <ottotruepro@gmail.com>
2020-01-18 18:29:15 +0700

    update readme

--- posts/forms.py
+++ posts/forms.py
@@ -4,3 +4,10 @@ from wtforms import Form, StringField, TextAreaField
 class PostForm(Form):
     title = StringField('Title')
     body = TextAreaField('Body')
+
+class YdForm(Form):
+    Nmin = StringField('Nmin')
+    Nmax = StringField('Nmax')
+    Ncur = StringField('Ncur')
+    user_requests = TextAreaField('Искомые слова')
+    results = TextAreaField('Результат', id='results')
