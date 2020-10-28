2a7623d708ff56cc6b7d253068a17ef39cc7773e
Ray <ottotruepro@gmail.com>
2020-10-28 15:47:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,18 @@
 import os
 
+from environs import Env
+
+env = Env()
+env.read_env()
+
 
 class Configuration(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
     SECRET_KEY = os.environ['SECRET_KEY']
+
     SECURITY_PASSWORD_SALT = 'salt'
     SECURITY_PASSWORD_HASH = 'sha512_crypt'
+
+    DEBUG = os.environ['DEBUG']
