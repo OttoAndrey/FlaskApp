1fc8dadf53656e3fbfaefeeeacb81713b8998be0
Ray <ottotruepro@gmail.com>
2020-10-28 15:42:49 +0700

    Remove yd_forms_parser

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -2,59 +2,13 @@ from flask import Blueprint, jsonify
 from flask import render_template
 from models import Post, Tag
 from flask import request
-from .forms import PostForm, YdForm
+from .forms import PostForm
 from app import db
 from flask import redirect
 from flask import url_for
 from flask_security import login_required
-from bs4 import BeautifulSoup
-import requests
+
 posts = Blueprint('posts', __name__, template_folder='templates')
-
-
-@posts.route('/yd_forms_parser', methods=['POST', 'GET'])
-def yd_forms_parser():
-    form = YdForm()
-
-    if request.method == 'POST':
-
-        Nmin = request.form['Nmin']
-        Nmax = request.form['Nmax']
-        Ncur = request.form['Ncur']
-        user_requests = request.form['user_requests']
-        results = {}
-        results['result'] = ''
-        # Очищаем пользовательсикие запросы от \n, \r и пустых
-        temp = user_requests.split('\n')
-        requests_clear = []
-        for r in range(0, len(temp)):
-            if temp[r] != '' and temp[r] != '\r':
-                requests_clear.append(temp[r].replace('\r', ''))
-
-        for N in range(int(Nmin), int(Nmax)+1):
-            Ncur = N
-            url = f'https://forms.yandex.ru/surveys/{N}/'
-            html = requests.get(url)
-            soup = BeautifulSoup(html.text, 'html.parser')
-            
-            if soup.find('h1') is not None:
-                title = soup.find('h1').text
-                print(title)
-                for r_с in requests_clear:
-                    if r_с.lower() in title.lower():
-                        results['result'] += url + '\n'
-                        print(results)
-
-                        return jsonify(results)
-
-        # form.Nmin.data = Nmin
-        # form.Nmax.data = Nmax
-        # form.Ncur.data = Ncur
-        # form.user_requests.data = user_requests
-        #
-        # form.results.data = results
-        # return render_template('posts/yd_forms_parser.html', form=form)
-    return render_template('posts/yd_forms_parser.html', form=form)
 
 
 @posts.route('/create', methods=['POST', 'GET'])
--- posts/forms.py
+++ posts/forms.py
@@ -4,10 +4,3 @@ from wtforms import Form, StringField, TextAreaField
 class PostForm(Form):
     title = StringField('Title')
     body = TextAreaField('Body')
-
-class YdForm(Form):
-    Nmin = StringField('Nmin')
-    Nmax = StringField('Nmax')
-    Ncur = StringField('Ncur')
-    user_requests = TextAreaField('Искомые слова')
-    results = TextAreaField('Результат', id='results')
